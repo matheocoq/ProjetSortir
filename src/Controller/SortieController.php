aab016e61f887c27238cc1a649f5b59924f647af
Matheo COQUEL <matheo.coquel2021@campus-eni.fr>
2023-01-03 15:08:34 +0100

    filtre

--- src/Controller/SortieController.php
+++ src/Controller/SortieController.php
@@ -2,8 +2,11 @@
 
 namespace App\Controller;
 
+use App\Repository\SitesRepository;
 use App\Repository\SortiesRepository;
+use App\Repository\VilleRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -11,11 +14,66 @@ use Symfony\Component\Routing\Annotation\Route;
 class SortieController extends AbstractController
 {
     #[Route('/', name: 'sortie_liste')]
-    public function liste(SortiesRepository $sortiesRepository): Response
+    public function liste(SitesRepository $sitesRepository,SortiesRepository $sortiesRepository,Request $request): Response
     {  
+
         $sorties = $sortiesRepository->findAll();
+        $sites= $sitesRepository->findAll();
+        $siteRechercher="";
+        $contient=null;
+        $dateDebut=null;
+        $dateFin=null;
+        $orga=false;
+        $inscrit=false;
+        $nonInscrit=false;
+        $passer=false;
+        $dateValid=true;
+        if ($request->query->get("recherche_site")) {
+            if ($request->query->get("date_fin")!="" && $request->query->get("date_debut")!="" && $request->query->get("date_fin") < $request->query->get("date_debut")) {
+                $dateValid=false;
+            }
+            else {
+                $sorties=$sortiesRepository->findByRecherche($request->query,$this->getUser());
+            }
+          
+            if ($request->query->get("nom_site")!="") {
+                $siteRechercher=$request->query->get("nom_site");
+            }
+            if ($request->query->get("nom_sortie_contient")!="") {
+                $contient=$request->query->get("nom_sortie_contient");
+            }
+            if ($request->query->get("date_debut")!="") {
+                $dateDebut=$request->query->get("date_debut");
+            }
+            if ($request->query->get("date_fin")!="") {
+                $dateFin=$request->query->get("date_fin");
+            }
+            if ($request->query->get("sortie_orga")!=null) {
+                $orga=true;
+            }
+            if ($request->query->get("sortie_insc")!=null){
+                $inscrit=true;
+            }
+            
+            if ($request->query->get("sortie_n_insc")!=null) {
+                $nonInscrit=true;
+            }
+            if ($request->query->get("sortie_passee")!=null) {
+                $passer=true;
+            }
+        }
         return $this->render('sortie/sortieListe.html.twig', [
             'sorties' => $sorties,
+            'sites' => $sites,
+            'siteRechercher' =>$siteRechercher,
+            'contient' =>$contient,
+            'dateDebut' =>$dateDebut,
+            'dateFin' =>$dateFin,
+            'orga' =>$orga,
+            'inscrit' =>$inscrit,
+            'nonInscrit' =>$nonInscrit,
+            'passer' =>$passer,
+            'dateValid'=>$dateValid
         ]);
     }
 
